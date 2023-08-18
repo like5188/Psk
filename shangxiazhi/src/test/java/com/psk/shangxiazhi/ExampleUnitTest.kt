aa7964c9ef2e86fd0c04ebff729ec525fc30e806
like <260864866@qq.com>
2023-08-18 10:54:43 +0800

    完善

--- shangxiazhi/src/test/java/com/psk/shangxiazhi/ExampleUnitTest.kt
+++ shangxiazhi/src/test/java/com/psk/shangxiazhi/ExampleUnitTest.kt
@@ -13,11 +13,19 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() = runBlocking {
+
+    }
+
+    /**
+     * 测试 delay 实际延迟。
+     * 由于系统资源的调度损耗，延迟会比设置的值增加10多毫秒，所以延迟10多毫秒以下毫无意义，因为根本不可能达到。
+     */
+    private suspend fun testDelay() {
         withTimeout(1000) {
             var startTime = 0L
             repeat(100) {
                 startTime = System.currentTimeMillis()
-                delay(100)
+                delay(1)
                 println("耗时：${System.currentTimeMillis() - startTime} $it")
             }
         }
