7b2dbeaba63e0d36ef4dba100990820052db5531
like <260864866@qq.com>
2023-08-22 13:27:16 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/scene/SceneActivity.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/scene/SceneActivity.kt
@@ -44,7 +44,7 @@ class SceneActivity : AppCompatActivity() {
     }
 
     private fun startGame(scene: TrainScene) {
-        mViewModel.start(true, false, true, scene, resistanceInt = 1, passiveModule = true, timeInt = 1)
+        mViewModel.start(false, false, false, scene, resistanceInt = 1, passiveModule = true, timeInt = 1)
     }
 
     override fun onBackPressed() {
