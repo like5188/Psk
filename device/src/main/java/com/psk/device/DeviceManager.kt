d9466b2e5352b57d8d306d5ed2b5250d371ac3a8
like5188 <260864866@qq.com>
2023-08-27 12:13:33 +0800

    完善

--- device/src/main/java/com/psk/device/DeviceManager.kt
+++ device/src/main/java/com/psk/device/DeviceManager.kt
@@ -10,13 +10,14 @@ import com.psk.device.data.source.remote.ble.BleDataSourceFactory
  * 设备管理。使用本模块时，只需使用此工具类进行初始化，然后使用koin注入仓库来使用。
  * 注意：
  * 一、如果要添加新的蓝牙设备系列（非当前已经存在的系列：血压、血氧、心电、上下肢），那么需要以下步骤：
- * 1、新增对应实体类放到[com.psk.device.data.model]中。
- * 2、新增一个对应的Dao。
- * 3、在[com.psk.device.data.db.database.DeviceDatabase]类中新增相关方法和entities。
- * 4、新增一个DbDataSource。名称格式为：[DeviceType]DbDataSource；包名为：[com.psk.device.data.source.local.db]。
- * 5、新增一个BaseDataSource。名称格式为：Base[DeviceType]Datasource；包名为：[com.psk.device.data.source.remote]。
- * 6、新增一个DataSource。名称格式为：[扫描出来的蓝牙设备的名称前缀]_[DeviceType]Datasource；包名为：[com.psk.device.data.source.remote.ble]。
- * 7、新增一个Repository。名称格式为：[DeviceType]Repository；包名为：[com.psk.device.data.source]。
+ * 1、在[DeviceType]中新增一个设备类型。
+ * 2、新增对应实体类放到[com.psk.device.data.model]中。
+ * 3、新增一个对应的Dao。
+ * 4、在[com.psk.device.data.db.database.DeviceDatabase]类中新增相关方法和entities。
+ * 5、新增一个DbDataSource。名称格式为：[DeviceType]DbDataSource；包名为：[com.psk.device.data.source.local.db]。
+ * 6、新增一个BaseDataSource。名称格式为：Base[DeviceType]Datasource；包名为：[com.psk.device.data.source.remote]。
+ * 7、新增一个DataSource。名称格式为：[扫描出来的蓝牙设备的名称前缀]_[DeviceType]Datasource；包名为：[com.psk.device.data.source.remote.ble]。
+ * 8、新增一个Repository。名称格式为：[DeviceType]Repository；包名为：[com.psk.device.data.source]。
  *
  * 二、如果只是要添加新的蓝牙设备，那么需要以下步骤：
  * 1、新增一个DataSource。名称格式为：[扫描出来的蓝牙设备的名称前缀]_[DeviceType]Datasource；包名为：[com.psk.device.data.source.remote.ble]。
