eb4324bae6d07432920ead7a8590f5070ece7f40
like <260864866@qq.com>
2023-08-29 11:11:55 +0800

    完善

--- device/src/main/java/com/psk/device/data/source/ShangXiaZhiRepository.kt
+++ device/src/main/java/com/psk/device/data/source/ShangXiaZhiRepository.kt
@@ -69,6 +69,8 @@ class ShangXiaZhiRepository : KoinComponent, IRepository<ShangXiaZhi> {
     }
 
     /**
+     * 设置上下肢参数，设置好后，如果是被动模式，上下肢会自动运行
+     *
      * @param passiveModule     被动模式
      * @param timeInt           时间 5-30 min
      * @param speedInt          速度 5-60 rpm
