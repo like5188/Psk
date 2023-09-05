e1d706c92e9635b3db0349c54d9d4584bac6bf5f
like <260864866@qq.com>
2023-09-05 15:20:41 +0800

    使用Calendar获取年月

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryActivity.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryActivity.kt
@@ -7,6 +7,7 @@ import com.like.common.util.startActivity
 import com.psk.common.CommonApplication
 import com.psk.shangxiazhi.R
 import com.psk.shangxiazhi.databinding.ActivityHistoryBinding
+import java.util.Calendar
 
 /**
  * 历史界面
@@ -21,15 +22,32 @@ class HistoryActivity : AppCompatActivity() {
     private val mBinding: ActivityHistoryBinding by lazy {
         DataBindingUtil.setContentView(this, R.layout.activity_history)
     }
+    private val cal: Calendar by lazy {
+        Calendar.getInstance()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mBinding.tvTime.text = getTime()
         mBinding.ivLeft.setOnClickListener {
-
+            mBinding.tvTime.text = decrementMonthAndGet()
         }
         mBinding.ivRight.setOnClickListener {
-
+            mBinding.tvTime.text = addMonthAndGet()
         }
     }
 
+    private fun decrementMonthAndGet(): String {
+        cal.add(Calendar.MONTH, -1)
+        return getTime()
+    }
+
+    private fun addMonthAndGet(): String {
+        cal.add(Calendar.MONTH, 1)
+        return getTime()
+    }
+
+    private fun getTime(): String {
+        return "${cal.get(Calendar.YEAR)}年${cal.get(Calendar.MONTH) + 1}月"
+    }
 }
