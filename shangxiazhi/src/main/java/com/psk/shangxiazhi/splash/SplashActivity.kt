c1c4a80291da85abbb80e08e3fecb8edb452a25d
like <260864866@qq.com>
2023-09-05 13:36:36 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/splash/SplashActivity.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/splash/SplashActivity.kt
@@ -8,7 +8,6 @@ import com.psk.shangxiazhi.main.MainActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import kotlin.system.measureTimeMillis
 
 /**
  * 闪屏界面
@@ -19,14 +18,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycleScope.launch {
-            var isLogin: Boolean
-            val remain = 1000 - measureTimeMillis {
-                isLogin = mViewModel.isLogin(this@SplashActivity)
-            }
-            if (remain > 0) {
-                delay(remain)
-            }
-            if (isLogin) {
+            delay(1000)
+            if (mViewModel.isLogin(this@SplashActivity)) {
                 MainActivity.start()
             } else {
                 LoginActivity.start()
