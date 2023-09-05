511a66a1e857ffa09fb44db71a27dd9e88c5aecc
like <260864866@qq.com>
2023-09-05 12:59:57 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/splash/SplashActivity.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/splash/SplashActivity.kt
@@ -2,29 +2,31 @@ package com.psk.shangxiazhi.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
-import com.psk.shangxiazhi.R
-import com.psk.shangxiazhi.databinding.ActivitySplashBinding
 import com.psk.shangxiazhi.login.LoginActivity
 import com.psk.shangxiazhi.main.MainActivity
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.system.measureTimeMillis
 
 /**
  * 闪屏界面
  */
 class SplashActivity : AppCompatActivity() {
-    private val mBinding: ActivitySplashBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.activity_splash)
-    }
     private val mViewModel: SplashViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding
         lifecycleScope.launch {
-            if (mViewModel.isLogin(this@SplashActivity)) {
+            var isLogin = false
+            val remain = 2000 - measureTimeMillis {
+                isLogin = mViewModel.isLogin(this@SplashActivity)
+            }
+            if (remain > 0) {
+                delay(remain)
+            }
+            if (isLogin) {
                 MainActivity.start()
             } else {
                 LoginActivity.start()
