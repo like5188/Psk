4ccd0e2d79c9310bc419dcad76d9f54f749ccbb0
like <260864866@qq.com>
2023-09-08 13:26:27 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/util/LevelView.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/util/LevelView.kt
@@ -1,9 +1,14 @@
 package com.psk.shangxiazhi.util
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.children
 import com.like.common.util.dp
 import com.psk.shangxiazhi.R
@@ -12,10 +17,14 @@ class LevelView(context: Context, attrs: AttributeSet) : LinearLayout(context, a
     private var min: Int = 0
     private var max: Int = 0
     private var step: Int = 0
-    private var cur: Int = 0
+    private val curLevel: Int = 0
+    private lateinit var lessView: AppCompatImageView
+    private lateinit var desView: TextView
+    private lateinit var addView: AppCompatImageView
 
     init {
         orientation = HORIZONTAL
+        gravity = Gravity.CENTER_VERTICAL
         val a = context.obtainStyledAttributes(attrs, R.styleable.LevelView)
         try {
             min = a.getInt(R.styleable.LevelView_min, 0)
@@ -33,7 +42,13 @@ class LevelView(context: Context, attrs: AttributeSet) : LinearLayout(context, a
         if (step <= 0) {
             throw IllegalArgumentException("LevelView count is invalid")
         }
-        val count = (max - min) / step + 1
+        addLevelView((max - min) / step + 1)
+        addLessView()
+        addDesView()
+        addAddView()
+    }
+
+    private fun addLevelView(count: Int) {
         repeat(count) { curIndex ->
             val view = View(context)
             view.setBackgroundResource(R.drawable.background_control_selector_commend_color)
@@ -42,11 +57,42 @@ class LevelView(context: Context, attrs: AttributeSet) : LinearLayout(context, a
                     view.isSelected = index <= curIndex
                 }
             }
-            val lp = LayoutParams(10.dp, 40.dp)
-            if (curIndex > 0) {
-                lp.marginStart = 10.dp
+            LayoutParams(10.dp, 40.dp).apply {
+                if (curIndex > 0) {
+                    marginStart = 10.dp
+                }
+                addView(view, this)
             }
-            addView(view, lp)
+        }
+    }
+
+    private fun addLessView() {
+        lessView = AppCompatImageView(context)
+        lessView.setImageResource(R.drawable.less_enable)
+        LayoutParams(30.dp, 30.dp).apply {
+            marginStart = 30.dp
+            addView(lessView, this)
+        }
+    }
+
+    private fun addDesView() {
+        desView = TextView(context)
+        desView.setTextColor(Color.parseColor("#00C4B9"))
+        desView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+        desView.gravity = Gravity.CENTER
+        desView.text = "3232"
+        LayoutParams(80.dp, LayoutParams.WRAP_CONTENT).apply {
+            marginStart = 5.dp
+            marginEnd = 5.dp
+            addView(desView, this)
+        }
+    }
+
+    private fun addAddView() {
+        addView = AppCompatImageView(context)
+        addView.setImageResource(R.drawable.add_enable)
+        LayoutParams(30.dp, 30.dp).apply {
+            addView(addView, this)
         }
     }
 
