53e150798d1cb263d7fd5ac4a2da29a9872b4d2c
like <260864866@qq.com>
2023-09-11 13:28:12 +0800

    去掉recovery、remotecontrol两个模块

--- recovery/src/main/java/com/psk/recovery/data/db/dao/MonitorDeviceDao.kt
+++
@@ -1,7 +0,0 @@
-package com.psk.recovery.data.db.dao
-
-import androidx.room.Dao
-import com.psk.recovery.data.model.MonitorDevice
-
-@Dao
-abstract class MonitorDeviceDao : BaseDao<MonitorDevice>()
--- recovery/src/main/java/com/psk/recovery/medicalorder/add/AddMedicalOrderUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.psk.recovery.medicalorder.add
-
-import com.like.common.util.mvi.Event
-import com.psk.common.util.ToastEvent
-
-data class AddMedicalOrderUiState(
-    val toastEvent: Event<ToastEvent>? = null,
-)
