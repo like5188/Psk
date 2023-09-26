9136ec63f469fdc9dafe5f4330feebb11655660d
like <260864866@qq.com>
2023-09-26 11:21:48 +0800

    完善

--- shangxiazhi/src/test/java/com/psk/shangxiazhi/ExampleUnitTest.kt
+++ shangxiazhi/src/test/java/com/psk/shangxiazhi/ExampleUnitTest.kt
@@ -1,9 +1,9 @@
 package com.psk.shangxiazhi
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
-import kotlin.system.measureTimeMillis
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,24 +13,39 @@ import kotlin.system.measureTimeMillis
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() = runBlocking {
-        val interval = 600L
-        val sendOrderInterval = 300L
-        val cost = measureTimeMillis {
-            if (interval < sendOrderInterval) {
-                delay(interval)
-            } else {
-                var remain = interval
-                while (remain > 0) {
-                    val d = remain.coerceAtMost(sendOrderInterval)
-                    delay(d)
-                    remain -= sendOrderInterval
-                    if (d >= sendOrderInterval) {
-                        println("握手")
+        val list = listOf(
+            A(0, 1),
+            A(1, 1),
+            A(3, 1),
+            A(4, 0),
+            A(5, 0),
+            A(5, 0),
+            A(6, 1),
+            A(8, 0),
+            A(9, 1),
+        )
+        var preMode = -1
+        var preModeStartSeconds = 0
+        var activeDuration = 0// 主动时长
+        var passiveDuration = 0// 被动时长
+        list.asFlow().collectLatest {
+            if (preMode != it.mode) {
+                when (it.mode) {
+                    0 -> {// 如果当前是主动，那么以前就是被动
+                        passiveDuration += it.seconds - preModeStartSeconds
+                    }
+
+                    1 -> {// 如果当前是被动，那么以前就是主动
+                        activeDuration += it.seconds - preModeStartSeconds
                     }
                 }
+                println("activeDuration=$activeDuration passiveDuration=$passiveDuration")
+                preMode = it.mode
+                preModeStartSeconds = it.seconds
             }
         }
-        println(cost)
     }
+
+    data class A(val seconds: Int, val mode: Int)
 
 }
