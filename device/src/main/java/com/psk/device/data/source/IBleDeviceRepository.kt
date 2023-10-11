b1fbabd519d5a379d3515e479d004cd6d16dafec
like <260864866@qq.com>
2023-10-11 16:44:14 +0800

    去掉ble模块，重构

--- device/src/main/java/com/psk/device/data/source/IBleDeviceRepository.kt
+++ device/src/main/java/com/psk/device/data/source/IBleDeviceRepository.kt
@@ -4,8 +4,8 @@ import kotlinx.coroutines.CoroutineScope
 
 interface IBleDeviceRepository<T> {
     /**
-     * 如果需要连接远端蓝牙设备，并且获取数据，必须调用此方法添加设备，然后才能使用bleManager进行相关操作。
-     * 如果只是需要获取数据库中缓存的数据，则不需要调用此方法。
+     * 如果需要连接远端蓝牙设备，并且获取数据，必须调用此方法启用设备，然后才能使用相关操作。
+     * 如果只是需要调用[getListByMedicalOrderId]方法需要获取数据库中缓存的数据，则不需要调用此方法。
      */
     fun enable(name: String, address: String)
 
