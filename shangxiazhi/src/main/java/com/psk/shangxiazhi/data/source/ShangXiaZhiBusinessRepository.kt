2814fee32cd768c6f22684038cdceedb8cbd184a
like <260864866@qq.com>
2023-11-13 15:13:08 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/data/source/ShangXiaZhiBusinessRepository.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/data/source/ShangXiaZhiBusinessRepository.kt
@@ -10,22 +10,18 @@ class ShangXiaZhiBusinessRepository(
     }
 
     fun login(serialNumber: String?, code: String?): Boolean {
-        return true
-        // todo
-//        loginDataSource.saveCode(code)
-//        return loginDataSource.login(serialNumber, code)
+        loginDataSource.saveCode(code)
+        return loginDataSource.login(serialNumber, code)
     }
 
     fun isLogin(context: Context): Boolean {
-        return true
-        // todo
-//        val serialNumber = loginDataSource.getSerialNumber(context)
-//        val code = loginDataSource.getCode()
-//        return try {
-//            loginDataSource.login(serialNumber, code)
-//        } catch (e: Exception) {
-//            false
-//        }
+        val serialNumber = loginDataSource.getSerialNumber(context)
+        val code = loginDataSource.getCode()
+        return try {
+            loginDataSource.login(serialNumber, code)
+        } catch (e: Exception) {
+            false
+        }
     }
 
 }
