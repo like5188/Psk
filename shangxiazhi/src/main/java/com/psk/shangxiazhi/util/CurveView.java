ce1193a9eedf11d6e18267e0c39db69bc971026c
like <260864866@qq.com>
2023-11-16 16:01:33 +0800

    修改图标绘制逻辑

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/util/CurveView.java
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/util/CurveView.java
@@ -32,10 +32,6 @@ import java.util.List;
  */
 
 public class CurveView extends RelativeLayout {
-
-
-    private ArrayList<Integer> entryList = new ArrayList<>();
-
     private LineChart mChart;
 
 
@@ -77,48 +73,9 @@ public class CurveView extends RelativeLayout {
             return;
         }
 
-        if (entryList.size() > 0) {
-            entryList.clear();
-        }
-
-        int size = list.size();
-
-        //初始化数据
-        if (size > 200) {
-            //取300个点的平均值
-            int count = size / 200;
-            System.out.println("count = " + count);
-            int totalSpeed = 0;
-
-            if (count == 1) {
-                for (int i = 0; i < size; i++) {
-                    entryList.add(list.get(i));
-                }
-            } else if (count >= 2) {
-                for (int i = 0; i < size; i++) {
-                    if (i % count == 0 && i != 0) {
-                        entryList.add(totalSpeed / (count - 1));
-                        totalSpeed = 0;
-                    } else {
-                        totalSpeed += list.get(i);
-                    }
-                }
-            }
-
-        } else {
-            for (int i = 0; i < size; i++) {
-                entryList.add(list.get(i));
-            }
-        }
-
-
         //初始化图表
         Description description = new Description();
-        if (size > 200) {
-            description.setText("时间/min");//设置描述文字内容
-        } else {
-            description.setText("时间/s");//设置描述文字内容
-        }
+        description.setText("时间/s");//设置描述文字内容
         description.setTextColor(MaterialColors.getColor(this, R.attr.colorSecondary));//设置描述文字的颜色
         description.setTextSize(10f);
         description.setXOffset(0f);
@@ -127,19 +84,16 @@ public class CurveView extends RelativeLayout {
         mChart.setDescription(description);//设置图表的描述文字，会显示在图表的右下角
         mChart.setTouchEnabled(false);//设置是否可触摸
         mChart.setNoDataText("当前数据为空");//设置当 chart 为空时显示的描述文字
-
 //        mChart.getDescription().setEnabled(false);//隐藏右下角描述
-
 
         //setChartData下面的"圈数" 的设置
         Legend legend = mChart.getLegend();
         legend.setEnabled(true);//设置Legend启用或禁用。 如果禁用， Legend 将不会被绘制。
-        legend.setFormSize(18f);
+        legend.setFormSize(12f);
         legend.setForm(Legend.LegendForm.CIRCLE);//样式，圆形
         legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         legend.setXOffset(3f);
         legend.setYOffset(3f);
-
 
         //X轴 建议隐藏，然后自定义一组展示出的数据,如：分钟0 5 10 15 20 30等
         XAxis xAxis = mChart.getXAxis();
@@ -148,17 +102,8 @@ public class CurveView extends RelativeLayout {
         xAxis.setTextSize(8);
         xAxis.setTextColor(MaterialColors.getColor(this, R.attr.colorSecondary));
         xAxis.setAxisMinimum(0);
-        if (size <= 200) {
-            xAxis.setAxisMaximum(size);
-            xAxis.setValueFormatter(null);
-
-        } else {
-            xAxis.setAxisMaximum(200);
-            xAxis.setLabelCount(size / 60 + 1, true);
-            xAxis.setValueFormatter(new MyXFormatter(size));
-        }
-
-//        xAxis.setAxisMaximum(200);
+        xAxis.setAxisMaximum(list.size());
+        xAxis.setValueFormatter(null);
 
         xAxis.setAxisLineWidth(2f);
         xAxis.setDrawGridLines(false);//是否展示网格线
@@ -180,14 +125,13 @@ public class CurveView extends RelativeLayout {
         yAxis.setAxisLineWidth(2f);
         yAxis.setDrawGridLines(true);//是否展示网格线
 
-
         //右侧，一般不用，设为false即可
         YAxis rightAxis = mChart.getAxisRight();
         rightAxis.setEnabled(false);
 
         ArrayList<Entry> entries = new ArrayList<>();
-        for (int i = 0; i < entryList.size(); i++) {
-            entries.add(new Entry(i, entryList.get(i)));
+        for (int i = 0; i < list.size(); i++) {
+            entries.add(new Entry(i, list.get(i)));
         }
 
         setChartData(entries, yLabel);
