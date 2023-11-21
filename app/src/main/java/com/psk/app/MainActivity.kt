ede6d98e0ad8477434ab0c1f00a8d80594d41cd9
like <260864866@qq.com>
2023-11-21 11:29:28 +0800

    完善

--- app/src/main/java/com/psk/app/MainActivity.kt
+++ app/src/main/java/com/psk/app/MainActivity.kt
@@ -81,7 +81,9 @@ class MainActivity : AppCompatActivity() {
                         // 注意：此处不能使用 onEach 进行每个数据的延迟，因为只要延迟，由于系统资源的调度损耗，延迟会比设置的值增加10多毫秒，所以延迟10多毫秒以下毫无意义，因为根本不可能达到。
                         // 这也导致1秒钟时间段内，就算延迟1毫秒，实际上延迟却会达到10多毫秒，导致最多只能发射60多个数据（实际测试）。
                         // 这就导致远远达不到心电仪的采样率的100多，从而会导致数据堆积越来越多，导致界面看起来会延迟很严重。
-                        coorYValues.toList().chunked(5).forEach {
+                        coorYValues.map {
+                            (-it) * 1000f// uV转mV。-t表示取反，因为如果不处理，画出的波形图是反的
+                        }.toList().chunked(5).forEach {
                             // 5个一组，125多的采样率，那么1秒钟发射25组数据就好，平均每个数据需要延迟40毫秒。
                             delay(1)
                             mBinding.ecgChartView.addData(it.toFloatArray())
