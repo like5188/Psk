bab60d595eaeb8ad82431fd71143497788762dc4
like <260864866@qq.com>
2023-11-24 14:59:03 +0800

    完善

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/game/business/HeartRateBusinessManager.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/game/business/HeartRateBusinessManager.kt
@@ -7,6 +7,7 @@ import com.psk.shangxiazhi.data.model.HeartRateReport
 import com.psk.shangxiazhi.data.model.IReport
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
@@ -37,7 +38,7 @@ class HeartRateBusinessManager(
 
     override suspend fun run() = withContext(Dispatchers.IO) {
         Log.d(TAG, "startHeartRateJob")
-        gameController.setEcgConfig(bleDeviceRepository.getSampleRate())
+//        gameController.setEcgConfig(bleDeviceRepository.getSampleRate())
         val flow = bleDeviceRepository.getFlow(this, medicalOrderId)
         launch {
             HeartRateReport.createForm(flow)
@@ -51,9 +52,17 @@ class HeartRateBusinessManager(
         }
         flow.filterNotNull().map {
             it.coorYValues
-        }.buffer(Int.MAX_VALUE).collect {
+        }.buffer(Int.MAX_VALUE).collect {coorYValues->
             // 取反，因为如果不处理，画出的波形图是反的
-            gameController.updateEcgData(it.map { -it }.toFloatArray())
+//            gameController.updateEcgData(coorYValues.map { -it }.toFloatArray())
+            // 注意：此处不能使用 onEach 进行每个数据的延迟，因为只要延迟，由于系统资源的调度损耗，延迟会比设置的值增加10多毫秒，所以延迟10多毫秒以下毫无意义，因为根本不可能达到。
+            // 这也导致1秒钟时间段内，就算延迟1毫秒，实际上延迟却会达到10多毫秒，导致最多只能发射60多个数据（实际测试）。
+            // 这就导致远远达不到心电仪的采样率的100多，从而会导致数据堆积越来越多，导致界面看起来会延迟很严重。
+            coorYValues.toList().chunked(5).forEach {
+                // 5个一组，125多的采样率，那么1秒钟发射25组数据就好，平均每个数据需要延迟40毫秒。
+                delay(32)
+                gameController.updateEcgData(it.toFloatArray())
+            }
         }
     }
 
