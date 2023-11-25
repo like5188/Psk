5000b351c3c8f6fe0c2a95e1e1e62a77d646a295
like <260864866@qq.com>
2023-11-25 14:53:39 +0800

    自定义心电图绘制

--- app/src/main/java/com/psk/app/EcgChartView.kt
+++ app/src/main/java/com/psk/app/EcgChartView.kt
@@ -31,6 +31,13 @@ import java.util.concurrent.SynchronousQueue
 import kotlin.math.ceil
 
 /*
+    SurfaceView 是一个可以在子线程中更新 UI 的 View，且不会影响到主线程。它为自己创建了一个窗口（window），就好像在视图层次（View Hierarchy）上穿了个“洞”，让绘图层（Surface）直接显示出来。但是，和常规视图（view）不同，它没有动画或者变形特效，一些 View 的特性也无法使用。
+    概括：
+    SurfaceView 独立于视图层次（View Hierarchy），拥有自己的绘图层（Surface），但也没有一些常规视图（View）的特性，如动画等。
+    SurfaceView 的实现中具有两个绘图层（Surface），即我们所说的双缓冲机制。我们的绘制发生在后台画布上，并通过交换前后台画布来刷新画面，可避免局部刷新带来的闪烁，也提高了渲染效率。
+    SurfaceView 中的 SurfaceHolder 是 Surface 的持有者和管理控制者。
+    SurfaceHolder.Callback 的各个回调发生在主线程
+
     实际心电图纸是由1mm*1mm的小方格组成，每1大格分为5小格
     横坐标代表时间，一般采用的走纸速度为25mm/s，每小格代表0.04s。
     纵坐标代表电压，一般采用的定标电压为1mV=10mm（10小格），每小格代表0.1mV。
