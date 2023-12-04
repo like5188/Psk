01d5691ac81fc1d32123a25e734c95886a6fa4b0
like <260864866@qq.com>
2023-12-04 16:35:21 +0800

    ExampleUnitTest.kt

--- app/src/test/java/com/psk/app/ExampleUnitTest.kt
+++ app/src/test/java/com/psk/app/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.psk.app
 
 import org.junit.Test
-
-import org.junit.Assert.*
+import kotlin.math.ceil
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +11,14 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val sampleRate = 250
+        val interval = 1000 / sampleRate// 绘制每个数据的间隔时间
+        val recommendInterval = 25// 建议循环间隔时间
+        val realInterval = interval.coerceAtLeast(recommendInterval)
+        val period = ceil(realInterval).toInt()// Math.ceil()向上取整
+        println("realInterval=$realInterval period=$period")
+        val circleTimes = 1000L / realInterval// 需要循环的次数
+        val numbersOfEachDraw = ceil(sampleRate / circleTimes).toInt()
+        println("circleTimes=$circleTimes numbersOfEachDraw=$numbersOfEachDraw")
     }
 }
