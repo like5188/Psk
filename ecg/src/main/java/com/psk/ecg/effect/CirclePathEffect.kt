6ed77b970c8843b298a4ce311c4f6218e4cbbad0
like <260864866@qq.com>
2024-02-03 15:37:18 +0800

    ecg

--- ecg/src/main/java/com/psk/ecg/effect/CirclePathEffect.kt
+++ ecg/src/main/java/com/psk/ecg/effect/CirclePathEffect.kt
@@ -9,9 +9,12 @@ import java.util.LinkedList
 class CirclePathEffect : IPathEffect {
     // 循环效果时，不需要画线的数据的index。即视图中看起来是空白的部分。
     private var spaceIndex = 0
+    private var isFull = false
+
     override fun handleData(data: Float, drawDataList: LinkedList<Float>, maxDataCount: Int) {
+        isFull = drawDataList.size == maxDataCount
         // 最多只绘制 maxDataCount 个数据
-        if (drawDataList.size == maxDataCount) {
+        if (isFull) {
             drawDataList.removeAt(spaceIndex)
             drawDataList.add(spaceIndex, data)
             spaceIndex++
@@ -24,10 +27,15 @@ class CirclePathEffect : IPathEffect {
     }
 
     override fun handlePath(path: Path, stepX: Float, index: Int, data: Float) {
-        if (index == spaceIndex || index == 0) {// == 0 使用moveTo是为了在绘制标准方波时，不让方波终点和路径起点连接起来
-            path.moveTo(index * stepX, data)// 达到空白效果
+        if (index == 0) {
+            // 非满数据状态，无需空白。只是需要使用moveTo是为了在绘制标准方波时，不让方波终点和路径起点连接起来
+            path.moveTo(0f, data)
         } else {
-            path.lineTo(index * stepX, data)
+            if (isFull && index == spaceIndex + 10) {// 满数据状态，需要空白效果
+                path.moveTo(index * stepX, data)
+            } else if (index !in spaceIndex..spaceIndex + 9) {
+                path.lineTo(index * stepX, data)
+            }
         }
     }
 
