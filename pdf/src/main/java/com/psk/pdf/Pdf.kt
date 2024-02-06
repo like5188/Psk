08f0656297df4b60f1b2ebf215ec98be00aca808
like <260864866@qq.com>
2024-02-06 13:52:48 +0800

    pdf

--- pdf/src/main/java/com/psk/pdf/Pdf.kt
+++ pdf/src/main/java/com/psk/pdf/Pdf.kt
@@ -54,59 +54,59 @@ class Pdf @JvmOverloads constructor(
      * 把 View 生成 PDF，自动根据 [paperSize] 进行分页
      */
     private suspend fun createPdfDocument(view: View): PdfDocument = withContext(Dispatchers.Main) {
-        // 计算总页数
-        var pageSize = 0
-        view.forEachPage { page, _, _, _, _ ->
-            pageSize = page
-        }
+        val pagers = view.splitPagers()
+        val pageSize = pagers.size
         // 创建 pdf 文档
-        val document = PdfDocument()
-        view.forEachPage { page, pageWidth, pageHeight, viewStartY, viewEndY ->
-            val pageInfo = PdfDocument.PageInfo.Builder(pageWidth, pageHeight, page).create()
-            document.startPage(pageInfo).apply {
-                // 页眉页脚
-                decorDrawer.drawDecor(canvas, pageWidth, pageHeight, page, pageSize, headerHeight, footerHeight)
-                // 截取内容区域
-                canvas.translate(0f, headerHeight.toFloat())
-                canvas.clipRect(Rect(0, 0, pageWidth, viewEndY - viewStartY))
-                // 平移使得view的绘制区域在canvas的内容区域上
-                canvas.translate(0f, -viewStartY.toFloat())
-                // 绘制内容
-                view.draw(canvas)
-                document.finishPage(this)
+        PdfDocument().apply {
+            pagers.forEach {
+                val pageInfo = PdfDocument.PageInfo.Builder(it.pageWidth, it.pageHeight, it.page).create()
+                startPage(pageInfo).apply {
+                    // 页眉页脚
+                    decorDrawer.drawDecor(canvas, it.pageWidth, it.pageHeight, it.page, pageSize, headerHeight, footerHeight)
+                    // 截取内容区域
+                    canvas.translate(0f, headerHeight.toFloat())
+                    canvas.clipRect(Rect(0, 0, it.pageWidth, it.viewEndY - it.viewStartY))
+                    // 平移使得view的绘制区域在canvas的内容区域上
+                    canvas.translate(0f, -it.viewStartY.toFloat())
+                    // 绘制内容
+                    view.draw(canvas)
+                    finishPage(this)
+                }
+                yield()
             }
-            yield()
         }
-        document
     }
 
-    private suspend fun View.forEachPage(onPage: suspend (page: Int, pageWidth: Int, pageHeight: Int, viewStartY: Int, viewEndY: Int) -> Unit) =
-        withContext(Dispatchers.Default) {
-            val pageWidth = width
-            val pageHeight = (pageWidth * paperSize.height.toFloat() / paperSize.width.toFloat()).toInt()
-            val splitLines = split.getSplitLines(this@forEachPage)
-            if (splitLines.isEmpty()) {
-                onPage(1, pageWidth, pageHeight, 0, pageHeight - headerHeight - footerHeight)
-                return@withContext
+    private suspend fun View.splitPagers(): List<Pager> = withContext(Dispatchers.Default) {
+        val pageWidth = width
+        val pageHeight = (pageWidth * paperSize.height.toFloat() / paperSize.width.toFloat()).toInt()
+        val pagers = mutableListOf<Pager>()
+        val splitLines = split.getSplitLines(this@splitPagers)
+        if (splitLines.isEmpty()) {
+            pagers.add(Pager(1, pageWidth, pageHeight, 0, pageHeight - headerHeight - footerHeight))
+            return@withContext pagers
+        }
+        // 当前页码
+        var page = 1
+        // 内容开始的坐标。
+        var viewStartY = 0
+        splitLines.forEachIndexed { index, lineY ->
+            val nextChildBottom = splitLines.getOrNull(index + 1)
+            if (nextChildBottom == null) {// 到底了
+                pagers.add(Pager(page, pageWidth, pageHeight, viewStartY, lineY))
+                return@withContext pagers
             }
-            // 当前页码
-            var page = 1
-            // 内容开始的坐标。
-            var viewStartY = 0
-            splitLines.forEachIndexed { index, lineY ->
-                val nextChildBottom = splitLines.getOrNull(index + 1)
-                if (nextChildBottom == null) {// 到底了
-                    onPage(page, pageWidth, pageHeight, viewStartY, lineY)
-                    return@withContext
-                }
-                val headerLineY = pageHeight * (page - 1) + headerHeight
-                val footerLineY = pageHeight * page - footerHeight
-                if (headerLineY + nextChildBottom - viewStartY > footerLineY) {// 当页已满
-                    onPage(page, pageWidth, pageHeight, viewStartY, lineY)
-                    viewStartY = lineY
-                    page++
-                }
+            val headerLineY = pageHeight * (page - 1) + headerHeight
+            val footerLineY = pageHeight * page - footerHeight
+            if (headerLineY + nextChildBottom - viewStartY > footerLineY) {// 当页已满
+                pagers.add(Pager(page, pageWidth, pageHeight, viewStartY, lineY))
+                viewStartY = lineY
+                page++
             }
         }
+        return@withContext pagers
+    }
+
+    class Pager(val page: Int, val pageWidth: Int, val pageHeight: Int, val viewStartY: Int, val viewEndY: Int)
 
 }
