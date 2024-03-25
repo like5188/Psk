f7d12459e7bb3052c78b80a002494ee3fd889819
like <260864866@qq.com>
2024-03-25 16:33:39 +0800

    compose

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryScreen.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryScreen.kt
@@ -1,19 +1,26 @@
 package com.psk.shangxiazhi.history
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -24,6 +31,7 @@ import com.psk.shangxiazhi.R
 import com.psk.shangxiazhi.customui.Bg
 import com.psk.shangxiazhi.customui.Title
 import com.psk.shangxiazhi.data.model.OrderInfo
+import java.text.SimpleDateFormat
 
 @Preview(widthDp = 1920, heightDp = 1080)
 @Composable
@@ -89,8 +97,48 @@ fun HistoryScreen(
                     )
                     Spacer(modifier = Modifier.width(40.dp))
                 }
+                List(orderInfoList)
             }
         }
     }
 
+}
+
+@Composable
+private fun List(orderInfoList: List<OrderInfo>? = null) {
+    if (orderInfoList.isNullOrEmpty()) {
+        return
+    }
+    val sdf by remember {
+        mutableStateOf(SimpleDateFormat("MM月dd日"))
+    }
+    val sdf1 by remember {
+        mutableStateOf(SimpleDateFormat("HH:mm:ss"))
+    }
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        for (orderInfo in orderInfoList) {
+            item {
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp, vertical = 30.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        Text(
+                            text = sdf.format(orderInfo.createTime),
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineSmall,
+                        )
+                        Text(
+                            text = sdf1.format(orderInfo.createTime),
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineSmall,
+                        )
+                    }
+                    Divider()
+                }
+            }
+        }
+    }
 }
