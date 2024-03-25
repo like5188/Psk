a80ce06fba4a21d4575a87f33e049f2ef6460aa2
like <260864866@qq.com>
2024-03-25 16:22:05 +0800

    compose

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryScreen.kt
+++ shangxiazhi/src/main/java/com/psk/shangxiazhi/history/HistoryScreen.kt
@@ -1,15 +1,26 @@
 package com.psk.shangxiazhi.history
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.psk.shangxiazhi.R
 import com.psk.shangxiazhi.customui.Bg
 import com.psk.shangxiazhi.customui.Title
 import com.psk.shangxiazhi.data.model.OrderInfo
@@ -32,7 +43,7 @@ private fun HistoryScreenPreview() {
  */
 @Composable
 fun HistoryScreen(
-    showTime: String? = null,
+    showTime: String = "",
     orderInfoList: List<OrderInfo>? = null,
 ) {
     Bg(contentAlignment = Alignment.Center) {
@@ -45,8 +56,40 @@ fun HistoryScreen(
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Title("选择训练时间")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .background(MaterialTheme.colorScheme.onSurfaceVariant),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Spacer(modifier = Modifier.width(40.dp))
+                    Icon(
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(40.dp),
+                        contentDescription = null,
+                        painter = painterResource(id = R.drawable.ic_left_arrow),
+                        tint = MaterialTheme.colorScheme.surface
+                    )
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = showTime,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.headlineSmall,
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(40.dp),
+                        contentDescription = null,
+                        painter = painterResource(id = R.drawable.ic_right_arrow),
+                        tint = MaterialTheme.colorScheme.surface
+                    )
+                    Spacer(modifier = Modifier.width(40.dp))
+                }
             }
-
         }
     }
 
