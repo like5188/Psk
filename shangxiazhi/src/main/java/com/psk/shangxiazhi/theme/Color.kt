701d0c41097c68b99bb263e6eea131d3ae31a8db
like <260864866@qq.com>
2024-03-25 10:04:54 +0800

    compose

--- shangxiazhi/src/main/java/com/psk/shangxiazhi/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.psk.shangxiazhi.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFF0058A2)
-val Purple500 = Color(0xFF0058A2)
-val Purple700 = Color(0xFF0058A2)
-val Teal200 = Color(0xFFFED612)
