3450eb9fe31db9fca095037727f7eb9e99947bd1
like <260864866@qq.com>
2024-03-28 16:00:02 +0800

    添加6分钟模块

--- sixminutes/src/main/java/com/psk/sixminutes/HeartRateBusinessManager.kt
+++ sixminutes/src/main/java/com/psk/sixminutes/HeartRateBusinessManager.kt
@@ -14,28 +14,28 @@ import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicBoolean
 
 class HeartRateBusinessManager {
-    private val heartRateRepository = DeviceRepositoryManager.createBleDeviceRepository<HeartRateRepository>(DeviceType.HeartRate)
-    private var heartRateJob: Job? = null
+    private val repository = DeviceRepositoryManager.createBleDeviceRepository<HeartRateRepository>(DeviceType.HeartRate)
+    private var job: Job? = null
     private val isInitialized = AtomicBoolean(false)
 
     fun init(context: Context, name: String, address: String) {
         if (isInitialized.compareAndSet(false, true)) {
-            heartRateRepository.init(context, name, address)
+            repository.init(context, name, address)
         }
     }
 
     fun getSampleRate(): Int {
         checkInit()
-        return heartRateRepository.getSampleRate()
+        return repository.getSampleRate()
     }
 
     fun connect(context: Context, lifecycleScope: LifecycleCoroutineScope, onResult: (List<List<Float>>) -> Unit) {
         checkInit()
         lifecycleScope.launch {
-            heartRateRepository.connect(lifecycleScope, 0L, {
+            repository.connect(lifecycleScope, 0L, {
                 context.showToast("心电仪连接成功")
-                heartRateJob = lifecycleScope.launch {
-                    heartRateRepository.fetch().filterNotNull().map {
+                job = lifecycleScope.launch {
+                    repository.fetch().filterNotNull().map {
                         it.coorYValues
                     }.buffer(Int.MAX_VALUE).collect {
                         // 取反，因为如果不处理，画出的波形图是反的
@@ -44,18 +44,18 @@ class HeartRateBusinessManager {
                 }
             }) {
                 context.showToast("心电仪连接失败")
-                heartRateJob?.cancel()
-                heartRateJob = null
+                job?.cancel()
+                job = null
             }
         }
     }
 
     fun disconnect() {
-        heartRateJob?.cancel()
-        heartRateJob = null
+        job?.cancel()
+        job = null
         // 避免未初始化时调用报错
         try {
-            heartRateRepository.close()
+            repository.close()
         } catch (e: Exception) {
         }
     }
