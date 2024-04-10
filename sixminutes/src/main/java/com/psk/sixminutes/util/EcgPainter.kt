aa349bc57cb0e88d01aa6914814fe9a54411a87b
like <260864866@qq.com>
2024-04-10 10:22:09 +0800

    完善

--- sixminutes/src/main/java/com/psk/sixminutes/util/EcgPainter.kt
+++ sixminutes/src/main/java/com/psk/sixminutes/util/EcgPainter.kt
@@ -9,32 +9,29 @@ import com.psk.ecg.painter.DynamicDataPainter
 import com.psk.ecg.painter.IBgPainter
 import com.psk.ecg.painter.IDataPainter
 
-fun createBgPainter(): IBgPainter = BgPainter(
-    Paint().apply {
-        color = Color.parseColor("#00a7ff")
-        strokeWidth = 2f
-        isAntiAlias = true
-        alpha = 120
-    },
-    Paint().apply {
-        color = Color.parseColor("#00a7ff")
-        strokeWidth = 1f
-        isAntiAlias = true
-        pathEffect = DashPathEffect(floatArrayOf(1f, 1f), 0f)
-        alpha = 90
-    },
-    null,
-    Paint().apply {
-        textSize = 18f
-        color = Color.RED
-    }
-)
+fun createBgPainter(): IBgPainter = BgPainter(Paint().apply {
+    color = Color.RED
+    strokeWidth = 1.5f
+    isAntiAlias = true
+//                alpha = 120
+}, Paint().apply {
+    color = Color.parseColor("#90FF0000")
+    strokeWidth = 1f
+    isAntiAlias = true
+    pathEffect = DashPathEffect(floatArrayOf(1f, 0f), 0f)
+//                alpha = 90
+}, null, Paint().apply {
+    textSize = 18f
+    color = Color.parseColor("#000000")
+    isAntiAlias = true
+})
 
 fun createDynamicDataPainter(): IDataPainter = DynamicDataPainter(
     CirclePathEffect(),
     Paint().apply {
-        color = Color.parseColor("#44C71E")
-        strokeWidth = 3f
+//                    color = Color.parseColor("#44C71E")
+        color = Color.parseColor("#000000")
+        strokeWidth = 2f
         style = Paint.Style.STROKE
         isAntiAlias = true
     }
